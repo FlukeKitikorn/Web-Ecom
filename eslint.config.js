d187f36838e74fac8c2db709c1cbe0118e8718eb
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2026-04-29 15:07:24 +0700

    chore : add global dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,24 @@
 const js = require("@eslint/js");
+const globals = require("globals");
 
 module.exports = [
   js.configs.recommended,
   {
     files: ["JS/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       "no-unused-vars": "warn",
-      "no-console": "off"
-    }
+      "no-console": "off",
+    },
   },
   {
     files: ["**/*.html"],
     plugins: {
-      html: require("eslint-plugin-html")
-    }
-  }
+      html: require("eslint-plugin-html"),
+    },
+  },
 ];
